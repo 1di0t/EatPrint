32a865b284f2309f2d07d994d3b5e2278b686c7a
전성일 <jeonjerry0709@gmail.com>
2024-08-11 23:41:00 +0900

    feat: add image picker

--- lib/screens/adding_page.dart
+++ lib/screens/adding_page.dart
@@ -1,16 +1,65 @@
+import 'dart:io';
+
 import 'package:eat_print/widget/app_bar_widget.dart';
+
 import 'package:flutter/material.dart';
 
-class AddingPage extends StatelessWidget {
+import 'package:image_picker/image_picker.dart';
+
+class AddingPage extends StatefulWidget {
   const AddingPage({super.key});
 
   @override
+  State<AddingPage> createState() => _AddingPageState();
+}
+
+class _AddingPageState extends State<AddingPage> {
+  File? _image;
+  final ImagePicker picker = ImagePicker();
+
+  Future<void> _pickImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {}
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Welcome to the Adding Page!'),
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          const SizedBox(height: 50),
+          //top bar
+          //picture
+          Expanded(
+            child: GestureDetector(
+                onTap: _pickImage,
+                child: Container(
+                  alignment: Alignment.center,
+                  child: _image == null
+                      ? Icon(
+                          Icons.add,
+                          size: 50,
+                          color: Colors.grey[800],
+                        )
+                      : ClipRRect(
+                          child: Image.file(
+                            _image!,
+                            width: double.infinity,
+                            height: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                )),
+          )
+          //gallery
+        ],
       ),
-      bottomNavigationBar: AppBarWidget(),
+      bottomNavigationBar: const AppBarWidget(),
     );
   }
 }
