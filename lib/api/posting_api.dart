132c66f08db11b26fc0384d8beef5f705942c23b
전성일 <jeonjerry0709@gmail.com>
2024-08-19 00:54:47 +0900

    feat: add posting function

--- lib/api/posting_api.dart
+++ lib/api/posting_api.dart
@@ -1,29 +1,64 @@
+import 'dart:convert';
+
 import 'package:eat_print/constants.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart'; // Add this import statement
 
 import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-Future<void> uploadImages(List<XFile> images) async {
-  for (var image in images) {
-    await _uploadImage(image);
+Future<int> uploadPost(
+    List<XFile> images, String content, latitude, longitude) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  int userNum = prefs.getInt('user_num') ?? 0;
+  List<String> imageUrls = await uploadImages(images);
+  final http.Response response;
+  try {
+    response = await http.post(
+      Uri.parse('$baseURL/post'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode(<String, dynamic>{
+        'user_num': userNum,
+        'imageUrls': jsonEncode(imageUrls),
+        'content': content,
+        'location': {
+          'latitude': latitude,
+          'longitude': longitude,
+        },
+        'hashtags': jsonEncode([]),
+      }),
+    );
+  } catch (e) {
+    return 500;
   }
+  return response.statusCode;
 }
 
-Future<void> _uploadImage(XFile imageFile) async {
+Future<List<String>> uploadImages(List<XFile> images) async {
+  //upload image in parallel
+  List<Future<String>> uploadFutures =
+      images.map((image) => _uploadImage(image)).toList();
+
+  List<String> imageUrls = await Future.wait(uploadFutures);
+
+  return imageUrls;
+}
+
+Future<String> _uploadImage(XFile imageFile) async {
   var request = http.MultipartRequest(
     'POST',
     Uri.parse('$baseURL/upload'),
   );
 
-  // XFile에서 파일 스트림을 가져와 MultipartFile 생성
   request.files.add(
     http.MultipartFile(
       'image',
-      imageFile.readAsBytes().asStream(), // 파일을 바이트 스트림으로 읽기
-      await imageFile.length(), // 파일 크기를 가져옴
-      filename: imageFile.name, // 파일 이름 설정
-      contentType: MediaType('image', 'jpeg'), // MIME 타입 설정
+      imageFile.readAsBytes().asStream(),
+      await imageFile.length(),
+      filename: imageFile.name,
+      contentType: MediaType('image', 'jpeg'),
     ),
   );
 
@@ -32,18 +67,8 @@ Future<void> _uploadImage(XFile imageFile) async {
 
   if (response.statusCode == 201) {
     var responseData = await http.Response.fromStream(response);
-    print('Image URL: ${responseData.body}');
+    return responseData.body;
   } else {
-    print('Failed to upload image. Status code: ${response.statusCode}');
+    return response.statusCode.toString();
   }
 }
-
-// Future<void> postingPost() async {
-//   final response = await http.post(
-//     Uri.parse('$baseURL/posting'),
-//     headers: <String, String>{
-//       'Content-Type': 'application/json; charset=UTF-8'
-//     },
-//     body: jsonEncode(<String, String>{}),
-//   );
-// }
